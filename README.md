8d84ce7fdf343cb4805e8b99f1520e00078653f7
redgvin <igor4ina@gmail.com>
2016-04-24 18:09:19 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebTorrentTestApp
+https://gist.github.com/iHaiduk/3176a138b7593da7d0dd
---
+++ src/containers/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>WebTorrent Test App</h1>
+      </div>
+    )
+  }
+}
